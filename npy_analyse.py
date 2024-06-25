bfdb665f4af0125f9f0ceb874d0369a2229447d7
zhewen-wenxuan <18858588688@163.com>
2024-06-25 13:28:36 +0800

    npy

--- npy_analyse.py
+++ npy_analyse.py
@@ -10,14 +10,29 @@ barcodes = np.load('/path/to/barcodes.npy')
 # 创建 AnnData 对象
 adata = ad.AnnData(X=expression_matrix, var={'gene_names': genes}, obs={'cell_barcodes': barcodes})
 
-# 查看 AnnData 对象
-print(adata)
+# 计算质量控制指标
+sc.pp.calculate_qc_metrics(adata, inplace=True)
 
-# 保存为 .h5ad 文件
-adata.write('/path/to/expression_matrix.h5ad')
+# 过滤掉低于一定阈值的基因和细胞
+sc.pp.filter_cells(adata, min_genes=200)
+sc.pp.filter_genes(adata, min_cells=3)
 
-# 以后可以直接加载 .h5ad 文件
-adata = sc.read_h5ad('/path/to/expression_matrix.h5ad')
+# 对数转换和标准化
+sc.pp.normalize_total(adata, target_sum=1e4)
+sc.pp.log1p(adata)
 
-# 查看数据
-print(adata)
+# 选择高度变异的基因
+sc.pp.highly_variable_genes(adata, n_top_genes=2000)
+adata = adata[:, adata.var['highly_variable']]
+
+# 数据缩放
+sc.pp.scale(adata, max_value=10)
+
+# PCA降维
+sc.tl.pca(adata, svd_solver='arpack')
+
+# 可视化 PCA 结果
+sc.pl.pca(adata, color='gene_names')
+
+# 保存处理后的数据
+adata.write('/path/to/processed_expression_matrix.h5ad')
